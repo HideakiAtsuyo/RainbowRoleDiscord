7f7c3f89a05bfa87a449c355789c3e323ddd9c99
Dany <53636771+Dany-LF@users.noreply.github.com>
2020-04-30 01:10:13 +0200

    Create bot.bat
    For helping people

---
+++ bot.bat
@@ -0,0 +1,72 @@
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+	
+@echo off
+cls
+color 5F
+:question
+echo 1) Install modules
+echo 2) Launch bot(if he crash you have to relaunch it)
+echo 3) Launch bot(if he crash he relaunch itself)
+echo 4) Exit
+set /p choix=What do u want? (1/2/3):
+ 
+if /I "%choix%"=="1" (goto :Install)
+if /I "%choix%"=="2" (goto :Launch)
+if /I "%choix%"=="3" (goto :LaunchWithoutStop)
+if /I "%choix%"=="4" (goto :End)
+goto question
+ 
+:Install
+cls
+echo.
+npm i
+echo Finished!
+goto question
+ 
+:Launch
+cls
+echo.
+echo Launched!
+node index.js
+pause
+goto end
+
+:LaunchWithoutStop
+cls
+echo.
+echo Launched!
+node index.js
+echo Crash... Relaunch(Wait 10 seconds)!!
+timeout /t 10 /nobreak
+goto LaunchWithoutStop
+ 
+:End
+cls
+echo.
+msg * Bye %username%
