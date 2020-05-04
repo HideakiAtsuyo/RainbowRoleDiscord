4b8b54419c1c48c3dc109e3cdda3eaaf603d9ad9
Dany <53636771+Dany-LF@users.noreply.github.com>
2020-05-04 03:42:43 +0200

    Missing ;
    Oops :)

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var TimeTooLowMsg = chalk.blue("<===============================================
 /////////////////////////////////////////////////
 client.on('ready', () => {
   console.clear();
-  client.setInterval(() => {var act = [{"text": "Playing with colors! :)","type": "PLAYING"},{"text": "By Dany-LF#6669","type": "WATCHING"},]try {const activity = act[Math.floor(Math.random() * act.length)];client.user.setActivity(activity.text, { type: activity.type });} catch (err) {return;}}, 15000);
+  client.setInterval(() => {var act = [{"text": "Playing with colors! :)","type": "PLAYING"},{"text": "By Dany-LF#6669","type": "WATCHING"},];try {const activity = act[Math.floor(Math.random() * act.length)];client.user.setActivity(activity.text, { type: activity.type });} catch (err) {return;}}, 15000);
     console.log(`----------------------------------------------------\n${colors.rainbow(figlet.textSync('Dany-LF'))}\n----------------------------------------------------\n--> ${chalk.blue('Name Bot            : ')}[ ${client.user.username} ]\n--> ${chalk.blue('Memory used         : ')}[ ${memoire} MB ]\n--> ${chalk.blue('RAM used            : ')}[ ${ram_1} on ${ram_2} ]\n----------------------------------------------------\n${(chalk.green('                      Ready !'))}\n----------------------------------------------------`);
 });
 
@@ -51,6 +51,6 @@ function TimeTooLow(){
   } else if(conf.StopIfTimeIsLessYhan5Seconds == false){
     console.log(TimeTooLowMsg);
   } else {
-  	return console.log("PUT true OR false for the config file for the StopIfTimeIsLessYhan5Seconds!")
+    return console.log("PUT true OR false for the config file for the StopIfTimeIsLessYhan5Seconds!")
   }
 };
